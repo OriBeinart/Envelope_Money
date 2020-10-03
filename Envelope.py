4ad45752f9ef5cda12a748ac4d7000a43fcacc56
OriBeinart <71370975+OriBeinart@users.noreply.github.com>
2020-10-03 17:38:25 +0300

    Update Envelope.py

--- Envelope.py
+++ Envelope.py
@@ -1,6 +1,6 @@
 import random #in order to cast a random sum of money for each envelope
 class Envelope():
-    def __init__(self, envelope_arr):
+    def __init__(self):
         self.money =  random.randint(100,000) #integer defining the sum of money the envelope contains
         self.used = False #boolian variable which states if the envelope was opened or not
     """Defines the values of Envelopes's properties.
